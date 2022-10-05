79d969d76c1374fd9d30ba29b1863d02d8b905ce
Julian Lord <juliandavidlord@gmail.com>
2022-10-05 13:36:39 +0200

    Population base added

---
+++ Population.java
@@ -0,0 +1,10 @@
+public class Population {
+
+    private final float elitismRatio;
+    private final float mutationRatio;
+    private final float crossoverRatio;
+    private Chromosome[] population;
+    
+    
+
+}
