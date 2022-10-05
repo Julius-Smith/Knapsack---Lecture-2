b1887f2d9a2c0e47732fc94c1c1670c4c836b48f
Julian Lord <juliandavidlord@gmail.com>
2022-10-05 13:34:28 +0200

    Added random chrom. generation

--- Utils.java
+++ Utils.java
@@ -2,4 +2,17 @@ public class Utils {
     public static float calculateFitness(Chromosome c) {
         return 0.0f;
     }
+    
+    public static Chromosome generateRandomChromosome(int chromosomeLength) {
+
+        ArrayList< Integer> gene = new ArrayList<>();
+
+        for(int i = 0; i < chromosomeLength; i++) {
+            gene.add(Configuration.INSTANCE.mersenneTwister.nextInt(1));
+        }
+        
+        Chromosome chromosome = new Chromosome(gene);
+
+        return chromosome;
+    }
 }
