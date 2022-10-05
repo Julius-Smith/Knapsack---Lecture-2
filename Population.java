ed5f6d53d86058c17a53471e460102c7bdae8cc3
Julian Lord <juliandavidlord@gmail.com>
2022-10-05 13:38:47 +0200

    Added some to population

--- Population.java
+++ Population.java
@@ -5,6 +5,17 @@ public class Population {
     private final float crossoverRatio;
     private Chromosome[] population;
     
-    
+    public Population(int size, float crossoverRatio, float elitismRatio, float mutationRatio) {
+        this.crossoverRatio = crossoverRatio;
+        this.elitismRatio = elitismRatio;
+        this.mutationRatio = mutationRatio;
+        population = new Chromosome[size];
+
+        for (int i = 0; i < size; i++) {
+            population[i] = Chromosome.generateRandom();
+        }
+
+        Arrays.sort(population);
+    }
 
 }
