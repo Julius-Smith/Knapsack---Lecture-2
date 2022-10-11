dc5eccfb5a88fe001f1feef8e5b55684eaf284db
Julius-Smith <81689210+Julius-Smith@users.noreply.github.com>
2022-10-11 10:40:40 +0200

    Iversion Mutation Done

--- InversionMutation.java
+++ InversionMutation.java
@@ -1,28 +1,51 @@
+import java.util.Arrays;
+import java.util.Collections;
+
 public class InversionMutation {
 
+    public static void main(String[] args){
+ 
+        int[] test = {2,4,5,7,8,6,1,9};
+        System.out.print(inversionMutation(test));
+    }
+
     
-    public static  int[] inversionMutation(int arr[]){
+    public static int[] inversionMutation(int arr[]){
         
-        int pivot1 = Configuration.INSTANCE.mersenneTwister.nextInt(arr.length);
-        int pivot2 = Configuration.INSTANCE.mersenneTwister.nextInt(arr.length);
-        
+        int pivot1 = Configuration.INSTANCE.mersenneTwister.nextInt(arr.length-1);
+        int pivot2 = Configuration.INSTANCE.mersenneTwister.nextInt(arr.length-1);
         
         if (pivot1 > pivot2){
             int temp = pivot2;
             pivot2 = pivot1;
             pivot1 = temp;
+        } else if (pivot1 == pivot2){
+            pivot2++;
         }
 
-        for(int i = pivot1; i <= pivot2; i++){
-            
-            if(arr[i] == 0){
-                arr[i] = 1;
-            } else{
-                arr[i] = 0;
-            }
+        int[] temp = Arrays.copyOfRange(arr, pivot1, pivot2);
+        int j = temp.length;
+        int [] temp2 = new int[j];
+
+        for (int i = 0; i < temp.length; i++) {
+            temp2[j - 1] = temp[i];
+            j = j - 1;
         }
         
-        return arr;
+        int[] genome = new int[arr.length];
+        for(int i = 0; i < pivot1; i++){
+            genome [i] = arr[i]; 
+        }
+        int count = 0;
+        for(int i = pivot1; i < pivot2; i++){
+            genome[i] = (temp2[count]);
+            count++;
+        }
+        for(int i = pivot2; i <= arr.length-1; i++){
+            genome[i] = arr[i];
+        }
+        
+        return genome;
     }
-
+    
 }
